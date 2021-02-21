f7dcb4dfa1c4424066926f4bd07a9a7aa12cf421
Hannah Hertzog <hhertzog@gmail.com>
2021-02-21 20:37:58 +0900

    adding jquery to fix dropdown issue; rerouting root to post index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'notifications/:id/link_through', to: 'notifications#link_through', as: :link_through
   devise_for :users
   resources :posts
-  root 'home#index'
+  root 'posts#index'
   get 'home/my_posts'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
